c0b6d7e8eefc96079792f70f9199e6d375e57fc6
Aliaksei Rudak <arudak@ebsco.com>
2024-05-18 13:23:25 +0500

    1

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,34 @@
 "use strict";
-var a = document.querySelector("#test");
-console.log(a === null || a === void 0 ? void 0 : a.innerHTML);
+var blackButtonClick = new CustomEvent("BBclick", {
+    bubbles: true,
+    detail: {
+        message: "HOBA"
+    }
+});
+var buttons = document.querySelectorAll(".button");
+buttons.forEach(function (btn) {
+    if (btn instanceof HTMLButtonElement)
+        if (btn.dataset.color === "black") {
+            btn.classList.add("black");
+            btn.dataset.userName = "VASYA";
+            btn.addEventListener("click", function () {
+                console.log("s");
+                btn.dispatchEvent(blackButtonClick);
+            });
+        }
+});
+var container = document.querySelector(".conatiner");
+container === null || container === void 0 ? void 0 : container.addEventListener("BBclick", function (e) {
+    console.log(e.target);
+});
+// buttons?.addEventListener("click", ({target}) => {
+//   if (target) {
+//     const eventTarget = target as HTMLButtonElement
+//   }
+// })
+// const input = document.querySelector("#input") as HTMLInputElement
+// input?.addEventListener("input", (e) => {
+//    console.log(e.target.value);
+//    console.log(e.target.getAttribute("value"));
+// })
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,5 +1,46 @@
 
 
-const a = document.querySelector("#test")
+const blackButtonClick = new CustomEvent("BBclick", {
+  bubbles: true,
+  detail: {
+    message: "HOBA"
+  }
+})
 
-console.log(a?.innerHTML);  
+const buttons = document.querySelectorAll(".button")
+
+buttons.forEach((btn) => {
+  if (btn instanceof HTMLButtonElement)
+
+
+  if (btn.dataset.color === "black") {
+    btn.classList.add("black")
+    btn.dataset.userName = "VASYA"
+    btn.addEventListener("click", () => {
+      console.log("s");
+      btn.dispatchEvent(blackButtonClick)
+    })
+  }
+})
+
+
+const container = document.querySelector(".conatiner")
+container?.addEventListener("BBclick", (e) => {
+  console.log(e.target);
+})
+// buttons?.addEventListener("click", ({target}) => {
+//   if (target) {
+//     const eventTarget = target as HTMLButtonElement
+//   }
+// })
+
+
+// const input = document.querySelector("#input") as HTMLInputElement
+// input?.addEventListener("input", (e) => {
+//    console.log(e.target.value);
+//    console.log(e.target.getAttribute("value"));
+
+// })
+
+
+
